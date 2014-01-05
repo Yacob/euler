72ad424997ad10bff4aa724ee376ab9d0d95e020
yacob <nestor.jac@gmail.com>
2014-01-04 19:17:49 -0500

    Add file p13.cpp with some comments

---
+++ proj11-20/p13.cpp
@@ -0,0 +1,15 @@
+//Project 13 (large sum)
+//find first ten digits of sum of the following 100 50-digit numbers
+
+//need to make something to add the numbers
+//store each number as char array and create new char array of numbers for addition
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	return 0;
+}
