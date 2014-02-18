ed52c5f502cab2a2a6ed539de2f7a3493b959734
yacob <nestor.jac@gmail.com>
2014-02-18 18:56:37 -0500

    Change p13.cpp to use GMP GMP is arbitrary precision arithmetic library for big numbers

--- proj11-20/p13.cpp
+++ proj11-20/p13.cpp
@@ -6,10 +6,42 @@
 
 #include <vector>
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <gmp.h>
+#include <gmpxx.h>
+
 
 using namespace std;
 
+
 int main(){
+
+	vector<string> numbers;
+	
+	ifstream infile;
+	infile.open("p13.txt");
+	while(infile.good()){	//read in text file of numbers
+		string nums = "";
+		getline(infile, nums);
+		numbers.push_back(nums);
+	}
+	numbers.pop_back();	//read in empty newline at end of p13.txt
+	mpz_class *bigNums = new mpz_class[numbers.size()];
+	for(int i = 0; i < numbers.size(); i++){
+		mpz_class num;
+		mpz_set_str(num.get_mpz_t(), numbers[i].c_str(), 10);
+		bigNums[i] = num;
+	}
+	
+	mpz_class total;
+	total = 0;
+	for(int i = 0; i < numbers.size(); i++){
+		mpz_add(total.get_mpz_t(), total.get_mpz_t(), bigNums[i].get_mpz_t());
+	}
+	
+	cout << total << endl;
 
 	return 0;
 }
